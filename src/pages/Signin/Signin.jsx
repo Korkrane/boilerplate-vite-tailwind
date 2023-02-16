21c9409111e390afd2d99073f0cdc68517524bf9
Korkrane <baudoin.haas@gmail.com>
2023-02-16 18:29:47 +0100

    add: custom alert to auth api calls

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,69 +1,56 @@
 
 import { SignLayout } from "@internals/layouts";
-import { useRef, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { AiFillGithub as GithubIcon } from 'react-icons/ai';
 import { FcGoogle as GoogleIcon} from 'react-icons/fc'
-import { FaFacebook as FacebookIcon} from 'react-icons/fa'
+import { FaFacebook as FacebookIcon } from 'react-icons/fa'
 import { RoundButtonWithIcon } from "./Components/RoundButtonWithIcon";
 import { Link } from "react-router-dom";
-
+import { FormField } from "./Components/FormField";
+import { FortyTwoIcon } from "../../assets/FortyTwoIcon";
+import axios from "axios";
+import { AlertContext } from "../../contexts/alert";
+import { Alert } from "./Components/Alert";
+import { API_BASE_URL } from '/src/api/api';
 // import Logo from '/src/assets/42logo.svg'
 // import { ReactComponent as Logo } from '/src/assets/42logo.svg';
 
 
-const FortyTwoIcon = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 -200 960 960"
-    xmlSpace="preserve"
-    width="1em"
-    height="1em"
-    style={{ fill: '#016e63' }}
-    {...props}
-  >
-    <path d="M32 412.6h330.1V578h164.7V279.1H197.3L526.8-51.1H362.1L32 279.1zM597.9 114.2 762.7-51.1H597.9zM762.7 114.2 597.9 279.1v164.8h164.8V279.1L928 114.2V-51.1H762.7z" />
-    <path d="M928 279.1 762.7 443.9H928z" />
-  </svg>
-)
-
-
-const FormField = ({ label, type, placeholder, value, onChange }) => {
-
-  return (
-    <label className="flex flex-col">
-      <span className="font-bold">{label}</span>
-      <input
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        className="p-1 bg-slate-100 rounded-md outline-slate-200/50"
-      />
-    </label>
-  );
-};
 
 export const Signin = () => {
 
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
+  const { setAlert } = useContext(AlertContext);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(`Submitted login: ${login} and password: ${password}`);
   };
 
+  const ThirdPartyAuthentication = (method) => {
+    console.log(method)
+    axios.post(`${API_BASE_URL}/auth/${method}`, {})
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        setAlert(error.message);
+      });
+  };
+
+  // make a layout to add Alert on everypage if it's needed
   return (
     <>
-      {/* <SignLayout> */}
+        <Alert type="error"/>
         <div className="w-full">
           <div className="flex flex-col">
-            <img className="w-6 h-6" src="/src/assets/42logo.svg"/>
+            <img className="w-6 h-6" src="/src/assets/42logo.svg" alt="42 logo"/>
             <div>Hypertube</div>
             <div>Unlimited streaming, endless entertainment</div>
           </div>
           <div className="bg-white rounded-lg h-fit w-fit shadow-2xl m-10 flex flex-col p-10">
-            <h1 className="font-bold text-2xl">Sign in to your account</h1>
+            <h1 className="font-bold text-2xl">Sign in</h1>
             <p>blablabla</p>
 
             <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
@@ -86,30 +73,27 @@ export const Signin = () => {
               </button>
             </form>
 
-            <Link to="/" className="group self-end text-blue-500 focus:outline-none transition duration-300 cursor-pointer">
+            <Link to="/" className="self-end text-blue-500 focus:outline-none focus:underline focus:decoration-blue-500 focus:decoration-2 focus:underline-offset-4 hover:underline-offset-4 hover:decoration-blue-500 hover:underline hover:decoration-2 cursor-pointer">
               Forgot your password?
-              <span class="block max-w-0 group-hover:max-w-full group-focus:max-w-full transition-all duration-500 h-1 bg-blue-500"></span>
             </Link>
 
             <div className="divider">OR</div>
             <div className="flex justify-center gap-2 mb-10">
-              <RoundButtonWithIcon icon={GoogleIcon}  />
-              <RoundButtonWithIcon icon={GithubIcon} />
-              <RoundButtonWithIcon icon={FacebookIcon} textColor="text-[#3b5998]"/>
-              <RoundButtonWithIcon icon={FortyTwoIcon}/>
+              <RoundButtonWithIcon icon={GoogleIcon}    name="Google login"   onClick={()=> ThirdPartyAuthentication('google')} />
+              <RoundButtonWithIcon icon={GithubIcon}    name="Github login"   onClick={()=> ThirdPartyAuthentication('github')} />
+              <RoundButtonWithIcon icon={FacebookIcon}  name="Facebook login" onClick={()=> ThirdPartyAuthentication('facebook')}  textColor="text-[#3b5998]"/>
+              <RoundButtonWithIcon icon={FortyTwoIcon}  name="42 login"       onClick={()=> ThirdPartyAuthentication('42')}/>
             </div>
 
-            <p>
+            <p className="self-end">
               Not registered ?{' '}
-              <Link to="/" className="group self-end text-blue-500 focus:outline-none transition duration-300 cursor-pointer">
+              <Link to="/" className=" text-blue-500 focus:outline-none focus:underline focus:decoration-blue-500 focus:decoration-2 focus:underline-offset-4 hover:underline-offset-4 hover:decoration-blue-500 hover:underline hover:decoration-2 cursor-pointer">
                 Create an account
-                <span class="block max-w-0 group-hover:max-w-full group-focus:max-w-full transition-all duration-500 h-1 bg-blue-500"></span>
               </Link>
             </p>
 
           </div>
         </div>
-      {/* </SignLayout> */}
     </>
   );
 }
